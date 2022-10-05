09524f45b34ad52c4660f06521f1f4481dfcf66a
mihaelashtereva <mihaelashtereva00@gmail.com>
2022-10-05 11:58:25 +0300

    CustomHealthCheck

---
+++ BookStore/BookStore/HealthChecks/CustomHealthCheck.cs
@@ -0,0 +1,32 @@
+﻿using BookStore.BL.Interfaces;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Diagnostics.HealthChecks;
+using System.Data.SqlClient;
+
+namespace BookStore.HealthChecks
+{
+    public class CustomHealthCheck : IHealthCheck
+    {
+        private readonly IAuthorService _authorService;
+
+        public CustomHealthCheck(IAuthorService authorService)
+        {
+            _authorService = authorService;
+        }
+
+        public async Task<HealthCheckResult> CheckHealthAsync(HealthCheckContext context, CancellationToken cancellationToken = default)
+        {            
+                try
+                {
+                    await _authorService.GetAllAuthors();
+                }
+                catch (SqlException e)
+                {
+                    return HealthCheckResult.Unhealthy(e.Message);
+                }
+            
+
+            return HealthCheckResult.Healthy("Author Service is OK");
+        }
+    }
+}
